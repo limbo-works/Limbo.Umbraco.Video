42df58326b6efda995192ed4c3d93b595065b9f6
Anders Bjerner <abjerner@skybrud.dk>
2022-03-23 12:21:40 +0100

    Introduced "VideoPackage" with constants about the package

---
+++ src/Limbo.Umbraco.Video/VideoPackage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Skybrud.Essentials.Reflection;
+using Umbraco.Cms.Core.Semver;
+
+namespace Limbo.Umbraco.Video {
+
+    /// <summary>
+    /// Static class with various information and constants about the package.
+    /// </summary>
+    public class VideoPackage {
+
+        /// <summary>
+        /// Gets the alias of the package.
+        /// </summary>
+        public const string Alias = "Limbo.Umbraco.Video";
+
+        /// <summary>
+        /// Gets the friendly name of the package.
+        /// </summary>
+        public const string Name = "Limbo Video";
+
+        /// <summary>
+        /// Gets the version of the package.
+        /// </summary>
+        public static readonly Version Version = typeof(VideoPackage).Assembly.GetName().Version;
+
+        /// <summary>
+        /// Gets the semantic version of the package.
+        /// </summary>
+        public static readonly SemVersion SemVersion = SemVersion.Parse(ReflectionUtils.GetInformationalVersion<VideoPackage>());
+
+        /// <summary>
+        /// Gets the URL of the GitHub repository for this package.
+        /// </summary>
+        public const string GitHubUrl = "https://github.com/limbo-works/Limbo.Umbraco.Video";
+
+        /// <summary>
+        /// Gets the URL of the issue tracker for this package.
+        /// </summary>
+        public const string IssuesUrl = "https://github.com/limbo-works/Limbo.Umbraco.Video/issues";
+
+        /// <summary>
+        /// Gets the URL of the documentation for this package.
+        /// </summary>
+        public const string DocumentationUrl = "https://github.com/limbo-works/Limbo.Umbraco.Video#documentation";
+
+
+    }
+
+}
